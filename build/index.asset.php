0fbc5c7bfc2e4b8d9e02d4ee8ea43a1a4587b9e0
Cburton555 <cburton5@my.wctc.edu>
2021-02-17 10:06:00 -0600

    Replaced files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '0e33184cc8d4577c5327ce048f8a0126');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '2f7c60fe59d34b1a889546787578751a');
